806ecadcea6b3b1e15113515bf500634885231ef
Suhas Kashyap <kashyapsuhas07@gmail.com>
2017-04-03 19:31:22 +0530

    Init after fail

---
+++ views/chat.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>login</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link rel="stylesheet" href="../public/stylesheets/bootstrap.min.css">
+		<link rel="stylesheet" href="../public/stylesheets/styles.css">
+		
+		<script src="../public/javascripts/jquery.min.js"></script>
+	</head>
+	<body>
+		<div class="container-fluid">
+		</div>
+	</body>
+</html>
