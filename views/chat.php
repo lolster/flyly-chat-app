c3b89d6a119bcdb0fcae2589b71628fc4731e675
Suhas Kashyap <kashyapsuhas07@gmail.com>
2017-04-04 22:18:38 +0530

    Basic design
    Broken mobile view though

--- views/chat.php
+++ views/chat.php
@@ -10,58 +10,105 @@
 		
 		<script src="../public/javascripts/jquery.min.js"></script>
 		<script>
-			function openNav() {
+			function open() {
 				document.getElementById("threads-list").style.width = "100%";
 				
 			}
-
-			function closeNav() {
+			function close() {
 				document.getElementById("threads-list").style.width = "0%";
+			}
+
+			// select particular thread for chatting
+			function selectThread() {
+				// TODO
+				console.log('poop 💩');
 			}
 		</script>
 	</head>
+
 	<body>
-		<br><br>
 		<div class="container">
-			<hr id="full-view" class="colorgraph">
 			<div class="row">
+				<!-- threads-list left -->
 				<div class="col-xs-12 col-sm-12 col-md-4" id="threads-list">
 					<div id="hamburger">
-						<span onclick="closeNav()">&#9776; close</span>
-						<hr class="colorgraph">
+						<span onclick="close()">&#9776; close</span>
 					</div>
-					<div class="well">
-						LEFT LEFT LEFT 
-					</div>
-					<div class="well">
-						LEFT LEFT LEFT 
-					</div>
-					<div class="well">
-						LEFT LEFT LEFT 
-					</div>
-					<div class="well">
-						LEFT LEFT LEFT 
+					<div id="search">
+						<input type="text" placeholder="search">
 					</div>
 					<hr class="colorgraph">
+
+					<!-- the threads -->
+					<div class="list">
+						<div class="threads" onclick="selectThread()">
+							<img class="img-circle" src="../public/images/profile.png">
+							<span class="name">Sushrith Arkal</span>
+							<span class="preview">I like macbook</span>
+						</div>
+						<div class="threads" onclick="selectThread()">
+							<img class="img-circle" src="../public/images/profile.png">
+							<span class="name">Sriharsha Hathwar</span>
+							<span class="preview">Hello 😃</span>
+						</div>
+						<div class="threads" onclick="selectThread()">
+							<img class="img-circle" src="../public/images/profile.png">
+							<span class="name">name</span>
+							<span class="preview">&nbsp;</span>
+						</div>
+						<div class="threads" onclick="selectThread()">
+							<img class="img-circle" src="../public/images/profile.png">
+							<span class="name">name</span>
+							<span class="preview">&nbsp;</span>
+						</div>
+						<div class="threads" onclick="selectThread()">
+							<img class="img-circle" src="../public/images/profile.png">
+							<span class="name">name</span>
+							<span class="preview">&nbsp;</span>
+						</div>
+						<div class="threads" onclick="selectThread()">
+							<img class="img-circle" src="../public/images/profile.png">
+							<span class="name">name</span>
+							<span class="preview">&nbsp;</span>
+						</div>
+						<div class="threads" onclick="selectThread()">
+							<img class="img-circle" src="../public/images/profile.png">
+							<span class="name">name</span>
+							<span class="preview">&nbsp;</span>
+						</div>
+						<div class="threads" onclick="selectThread()">
+							<img class="img-circle" src="../public/images/profile.png">
+							<span class="name">name</span>
+							<span class="preview">&nbsp;</span>
+						</div>
+						<div class="threads" onclick="selectThread()">
+							<img class="img-circle" src="../public/images/profile.png">
+							<span class="name">name</span>
+							<span class="preview">&nbsp;</span>
+						</div>
+						<div class="threads" onclick="selectThread()">
+							<img class="img-circle" src="../public/images/profile.png">
+							<span class="name">name</span>
+							<span class="preview">&nbsp;</span>
+						</div>
+					</div>
+
 				</div>
+				<!-- chat-area right -->
 				<div class="col-xs-12 col-sm-12 col-md-8" id="chat-area">
 					<div id="hamburger">
-						<span onclick="openNav()">&#9776; threads</span>
-						<hr class="colorgraph">
+						<span onclick="open()">&#9776; threads</span>
 					</div>
-					<div class="well">
-						--------RIGHT ----
-					</div>
-					<div class="well">
-						--------RIGHT ----
-					</div>
-					<div class="well">
-						--------RIGHT ----
-					</div>
-					<div class="well">
-						--------RIGHT ----
+					<div id="chat-area-header">
 					</div>
 					<hr class="colorgraph">
+
+					<!-- conversation area -->
+					<div id="conversation-area">
+						<div class="jumbotron text-center">
+							<h1>TODO</h1>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
