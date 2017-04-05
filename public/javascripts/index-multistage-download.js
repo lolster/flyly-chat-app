cbd43bfa378feaf25f7638c019843f6d31f3a298
Suhas Kashyap <kashyapsuhas07@gmail.com>
2017-04-05 20:21:33 +0530

    modified multi stage stuff to get local images

--- public/javascripts/index-multistage-download.js
+++ public/javascripts/index-multistage-download.js
@@ -1,5 +1,5 @@
 // put whatever urls here
-var imgSrc = ['http://i.imgur.com/QRADrXh.jpg', 'http://i.imgur.com/7ss7KpJ.jpg', 'https://i.redditmedia.com/S-5DrXu-oSY6rpP85mjTA-BUe6i1gILWBq6Ow3_WYdE.jpg?w=1024&s=be4490678cce011cea176c40e500e4bd', 'https://i.redditmedia.com/CxVKZbynn_bcoG6B5Fh8Lud1HSj_v0oQnzo1gmVMjT0.jpg?w=1024&s=aafa9c98cf784876d6f99b02df344275'];
+var imgSrc = ['../public/images/Elefantasia.jpg', '../public/images/Hanuman.jpg', '../public/images/Leonine.jpg', '../public/images/profile.png'];
 
 $(window).on('load', () => {
 	var imgList = $('img');
@@ -11,8 +11,9 @@ $(window).on('load', () => {
 		imgList[i].src = imgSrc[i];
 		imgList[i+1].src = imgSrc[i+1];
 		i += 2;
+		console.log(imgList);
 		console.log('yolo');
-		setTimeout(getImgs, 1000);
+		setTimeout(getImgs, 2000);
 	}
 	getImgs();
 });
