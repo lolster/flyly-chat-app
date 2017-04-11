79053ab7fed6d4552d4fc43fd61458e4ee0aba08
Sushrith Arkal <sushrith.arkal@gmail.com>
2017-04-11 18:52:59 +0530

    Updated with MySQL Integration
    Now updates the `messages` table before sending messages to users.

--- sock-int/index.js
+++ sock-int/index.js
@@ -14,7 +14,7 @@ const mysql = require('mysql');
 var con = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
-	password: '',
+	password: '123',
 	database: 'flyly'
 });
 
@@ -65,20 +65,84 @@ io.on('connection', (socket) => {
 		//sender id
 		var s = -1;
 		con.query('SELECT userid FROM users WHERE username = ?', [data.sender], function(error, results, fields) {
-			if(error) {
+			if(error || results.length == 0) {
 				console.log('[ERROR] Cannot get sender id');
 				return;
 			}
+			s = results[0].userid;
+			//recv id
+			var r = -1
+			con.query('SELECT userid FROM users WHERE username = ?', [data.receiver], function(error, results, fields) {
+				if(error || results.length == 0) {
+					console.log('[ERROR] Cannot get receiver id');
+					return;
+				}
+				r = results[0].userid;
+				console.log('[INFO] From database: ');
+				console.log('[INFO] Sender: ' + data.sender + ' id: ' + s);
+				console.log('[INFO] Receiver: ' + data.receiver + ' id: ' + r);
 
+				//insert into message table
+				var message = {
+					body: data.msg,
+					send_id: s,
+					rcv_id: r
+				};
+
+				con.query('INSERT INTO messages SET ?', message, function(err, res) {
+					if(err) {
+						console.log('[ERROR] Cannot insert message into database');
+						return;
+					}
+					console.log('[INFO] Inserted, insert id: ' + res.insertId);
+					console.log('[INFO] ' + 'Message: ' + data.msg);
+					socket.to(data.receiver).broadcast.emit('chat message', data);
+					console.log('\n\n');
+				});
+			});
+		});
+	});
+
+	socket.on('disconnect', () => {
+		console.log('a user disconnected');
+	});
+});
+
+/*
+// old stuff
+// technically doesn't work
+//socket.io stuff
+io.on('connection', (socket) => {
+	console.log('a user connected');
+
+	socket.on('change room', (data) => {
+		console.log(data.room);
+		socket.join(data.room);
+		//TODO update MySQL database to reflect that user is logged in and online
+	});
+
+	socket.on('chat message', (data) => {
+		//TODO Send data to mysql database as well before sending to user
+		
+		//get the id of sender and reciever
+		//sender id
+		var s = -1;
+		con.query('SELECT userid FROM users WHERE username = ?', [data.sender], function(error, results, fields) {
+			if(error || results.length == 0) {
+				console.log('[ERROR] Cannot get sender id');
+				return;
+			}
+			s = results[0].userid;
 		});
 		
 		//recv id
-		var r = -;1
+		var r = -1
 		con.query('SELECT userid FROM users WHERE username = ?', [data.receiver], function(error, results, fields) {
-			if(error) {
+			if(error || results.length == 0) {
 				console.log('[ERROR] Cannot get receiver id');
 				return;
 			}
+			r = results[0].userid;
 		});
 
 		console.log('[INFO] From database: ');
@@ -109,3 +173,4 @@ io.on('connection', (socket) => {
 		console.log('a user disconnected');
 	});
 });
+*/
