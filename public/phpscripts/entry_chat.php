9a090377362ab2632ebc723e091e3395a3141cc4
Sushrith Arkal <sushrith.arkal@gmail.com>
2017-04-16 17:50:04 +0530

    Fixed sorting of names based on timestamps of messages sent to user
    Mr. Hunter, why?

--- public/phpscripts/entry_chat.php
+++ public/phpscripts/entry_chat.php
@@ -4,14 +4,20 @@
 	//mysqli_report((MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 	require 'db_connect.php';
+	/*
 	$user = $_SESSION['username'];
 	$firstname = $_SESSION['firstname'];
 	$lastname = $_SESSION['lastname'];
 	$userid = $_SESSION['id'];
 	$email = $_SESSION['email'];
-
+	*/
+	$user = 'hunter';
+	$firstname = 'Sriharsha';
+	$lastname = 'Hatwar';
+	$userid = 10;
+	$email = 'whocares@gmail.com';
 	#Changes -> need to get the first name , lastname of a particular username
-	if(!$stmnt = $connection->prepare('SELECT username, firstname, lastname from users where userid in (select send_id from messages where rcv_id = ? )')){
+	if(!$stmnt = $connection->prepare('SELECT userid, username, firstname, lastname from users where userid in (select send_id from messages where rcv_id = ? )')){
 		 die(json_encode(array(
 			 'status'=>'error',
 			 'message'=>'query failed!'
@@ -30,13 +36,15 @@
 		)));
 	}
 	// variables to hold the result.
+	$userFriendId = -1;
 	$userFriend = '';
 	$userFriendFirstN = '';
 	$userFriendLastN = '';
+	$userFriendsId = array();
 	$userFriends = array();
 	$userFriendsFirstName = array();
 	$userFriendsLastName = array();
-	if(!$stmnt->bind_result($userFriend,$userFriendFirstN,$userFriendLastN)) {
+	if(!$stmnt->bind_result($userFriendId, $userFriend, $userFriendFirstN, $userFriendLastN)) {
 		die(json_encode(array(
 			'status'=>'error',
 			'msg'=>'binding failed'
@@ -44,6 +52,7 @@
 	}
 	while($stmnt->fetch()){
 		//array_push($userFriends,$user); //waste time and stack space by making function call
+		$userFriendsId[] = $userFriendId;
 		$userFriends[] = $userFriend;
 		$userFriendsFirstName[] = $userFriendFirstN;
 		$userFriendsLastName[] = $userFriendLastN;
@@ -60,7 +69,7 @@
 	
 	for($i = 0 ; $i < $length ; $i++){
 		//get the latest timestamp by querying.
-		$frID = $userFriends[$i];
+		$frID = $userFriendsId[$i];
 		$time_friend = "";
 		
 		if(!($stmnt = $connection->prepare('SELECT msgTime FROM messages WHERE ((send_id = ? AND rcv_id = ?) OR (send_id = ? AND rcv_id = ?)) ORDER BY msgTime DESC LIMIT 1'))) {
@@ -80,10 +89,12 @@
 		
 		if(!$stmnt->execute()){
 			die(json_encode(array(
-				'status'=>'error while execuiting second Query',
+				'status'=>'error while executing second query',
 				'message'=>'execution falied!'
 			)));
 		}
+
+		$time_friend = '';
 		
 		if(!$stmnt->bind_result($time_friend)){
 			die(json_encode(array(
@@ -91,20 +102,58 @@
 				'msg'=>'binding failed'
 			)));
 		}
+
+		$fml = $stmnt->fetch();
 		
-		$friend_wid_time[] = array('name'=>$userFriends[$i],'timestamp'=>$time_friend);
-		$stmnt->close();
+		if($fml === null) {
+			$friend_wid_time[] = array('name'=>$userFriends[$i],'timestamp'=>null);
+		}
+		else if($fml === false) {
+			die(json_encode(array(
+				'status'=>'error',
+				'msg'=>'binding failed'
+			)));	
+		}
+		else {
+			$friend_wid_time[] = array('name'=>$userFriends[$i],'timestamp'=>$time_friend);
+			$stmnt->close();
+		}
 	}
 	
 	function build_sorter($key){
 		return function($a,$b) use ($key){
-			return strnatcmp($a[$key] , $b[$key]);
+			$date1 = DateTime::createFromFormat('Y-m-d H:i:s', $a[$key]);
+			$date2 = DateTime::createFromFormat('Y-m-d H:i:s', $b[$key]);
+			$res = 0;
+			if ($date1 < $date2) $res = 1;
+			else if ($date1 > $date2) $res = -1;
+			return $res;
+			//return (($date1 < $date2) ? (-1 : (($date2 < $date1) ? ( 1 : 0 ))));
 		};
 	}
-	
+
 	usort($friend_wid_time, build_sorter('timestamp'));
-	
 	for($i = 0 ; $i < $length ; ++$i) {
+		// Mr. Hunter, why thou must force me to do this egregious deed?;
+		$index = array_search($friend_wid_time[$i]['name'], $userFriends);
+		echo $index;
+		if(!$index) {
+			die(json_encode(array(
+				'status' => 'serror',
+				'msg' => 'kill me'
+			)));
+		}
+		
+		$temp = $userFriendsFirstName[$i];
+		$userFriendsFirstName[$i] = $userFriendsFirstName[$index];
+		$userFriendsFirstName[$index] = $temp;
+		
+		$temp = $userFriendsLastName[$i];
+		$userFriendsLastName[$i] = $userFriendsLastName[$index];
+		$userFriendsLastName[$index] = $temp;
+		
+		$temp = $userFriends[$i];
 		$userFriends[$i] = $friend_wid_time[$i]['name'];
+		$userFriends[$index] = $temp;
 	}
 ?>
