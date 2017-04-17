7e08e8fd21797ceb89f6c388f2f12d6b5a1bbde1
Sushrith Arkal <sushrith.arkal@gmail.com>
2017-04-17 13:57:15 +0530

    Added some images
    much wow

--- public/javascripts/index-multistage-download.js
+++ public/javascripts/index-multistage-download.js
@@ -1,5 +1,5 @@
 // put whatever urls here
-var imgSrc = ['../public/images/dog.jpg', '../public/images/dog.jpg', '../public/images/dog.jpg', '../public/images/dog.jpg'];
+var imgSrc = ['../public/images/1.png', '../public/images/2.png', '../public/images/3.png', '../public/images/4.png', '../public/images/5.png', '../public/images/6.png', '../public/images/7.png', '../public/images/8.png', '../public/images/9.png'];
 
 $(window).on('load', () => {
 	var imgList = $('img');
